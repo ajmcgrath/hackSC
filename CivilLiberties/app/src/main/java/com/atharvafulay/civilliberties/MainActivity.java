3b81d91c549c396e690047329d953ebc221bc8af
Shelton Cai <shelton408@gmail.com>
2020-02-01 05:21:46 -0800

    minor changes, home button on second page functions now

--- CivilLiberties/app/src/main/java/com/atharvafulay/civilliberties/MainActivity.java
+++ CivilLiberties/app/src/main/java/com/atharvafulay/civilliberties/MainActivity.java
@@ -10,21 +10,52 @@ import android.view.View.OnClickListener;
 import android.widget.TextView;
 import android.os.Handler;
 import android.os.SystemClock;
-
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.util.Log;
+import android.support.v4.app.*;
+import androidx.core.app.ActivityCompat;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    //recording vars
+    private static final String LOG_TAG = "AudioRecordTest";
+    private static String fileName = null;
     private int recordState = 1;
+    private MediaRecorder recorder = null;
+    private boolean playing = false;
+    private MediaPlayer   player = null;
+    //timer vars
     int Seconds, Minutes, MilliSeconds ;
     long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L ;
     Handler handler;
 
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         handler = new Handler() ;
-        //record button code
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+        //home button code
         final Button copButton = findViewById(R.id.copButton);
         copButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,7 +63,32 @@ public class MainActivity extends AppCompatActivity {
                 openPulledOverInfo();
             }
         });
+        //profile button code
+        final Button profileButton = findViewById(R.id.profileButton);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (playing) {
+                    player.release();
+                    player = null;
+                    playing = false;
+                }
+                else {
+                    player = new MediaPlayer();
+                    try {
+                        player.setDataSource(fileName);
+                        player.prepare();
+                        player.start();
+                    } catch (IOException e) {
+                        Log.e(LOG_TAG, "prepare() failed");
+                    }
+                    playing = true;
+                }
+            }
+        });
         //record button code
+        fileName = getExternalCacheDir().getAbsolutePath();
+        fileName += "/pullover.3gp";
         final Button recordButton = findViewById(R.id.recordButton);
         recordButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,11 +104,13 @@ public class MainActivity extends AppCompatActivity {
                     MilliSeconds = 0 ;
                     handler.removeCallbacks(recordRunnable);
                     recordButton.setText(R.string.record_button);
+                    stopRecording();
                 }
                 else{
                     recordButton.setBackgroundColor(getResources().getColor(R.color.button_pressed));
                     StartTime = SystemClock.uptimeMillis();
                     handler.postDelayed(recordRunnable, 0);
+                    startRecording();
                 }
                 recordState++;
             }
@@ -62,6 +120,29 @@ public class MainActivity extends AppCompatActivity {
     public void openPulledOverInfo(){
         Intent intent = new Intent(this, pulledOverInfo.class);
         startActivity(intent);
+    }
+
+    //recording methods
+    private void startRecording() {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setOutputFile(fileName);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        recorder.start();
+    }
+
+    private void stopRecording() {
+        recorder.stop();
+        recorder.release();
+        recorder = null;
     }
     //Timer runnable
     public Runnable recordRunnable = new Runnable() {
--- CivilLiberties/app/src/main/java/com/atharvafulay/civilliberties/pulledOverInfo.java
+++ CivilLiberties/app/src/main/java/com/atharvafulay/civilliberties/pulledOverInfo.java
@@ -2,7 +2,10 @@ package com.atharvafulay.civilliberties;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class pulledOverInfo extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class pulledOverInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pulled_over_info);
+
+        final Button homeButton = findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHome();
+            }
+        });
+    }
+    public void openHome(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
